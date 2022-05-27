4483ea9cad456207568dc6f4e3963acbece18bc0
Real-Musafir <alshahadath@gmail.com>
2022-05-27 16:16:52 +0600

    send data by post method

--- client.js
+++ client.js
@@ -1,9 +1,41 @@
 const io = require("socket.io-client");
-const API_URL = "http://localhost:3001/";
-//  const API_URL = "http://192.168.0.102:3001/";
+const fetch = require("node-fetch");
+const API_URL = "http://localhost:3001";
+// const API_URL = "http://192.168.0.102:3001/";
 
-socket = io(`${API_URL}`);
-console.log("Yooooo");
-socket.on("connection");
+// socket = io(`${API_URL}`);
+// console.log("Yooooo");
+// socket.on("connection", (socket) => console.log("asdf"));
 
 // socket.emit("chat message", { msg: "asdf" });
+
+// fetch("http://localhost:3001")
+//   .then((res) => res.text())
+//   .then((text) => console.log(text));
+
+let todo = {
+  userId: 123,
+  title: "loren impsum doloris",
+  completed: false,
+};
+
+try {
+  let result = fetch("http://localhost:3001", {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "Shahadath",
+      password: "123456",
+    }),
+  });
+
+  result.then((success) => {
+    console.log(success);
+  });
+} catch (error) {
+  console.log(error);
+}
--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const io = require("socket.io")(server);
 
 const port = 3001;
 
+app.use(express.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(express.json());
+
 app.get("/", (req, res) => {
+  console.log("Really come here?");
   res.send("hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log("Is this post request");
+  console.log(req.body, "body find??");
+  res.send({ ok: "ok" });
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected :D");
-  console.log(socket);
+
   socket.on("chat message", (msg) => {
     console.log(msg);
     io.emit("chat message", msg);
