9744ffab2a5a03bebf9c5c5ef10fb2d8f19bf817
Real-Musafir <alshahadath@gmail.com>
2022-05-31 10:26:44 +0600

    set cors

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 app.set("port", process.env.PORT);
 
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+// const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 let channleName = "";
 
